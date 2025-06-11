d4b6bfdb7800ab2475b79217a256079b92414209
Yaan768 <yaan7682019@gmail.com>
2025-06-11 11:24:10 -0500

    Agregue el ejercicio 2: Primera parte, falta la funcion

---
+++ ejercicio2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//Aca debe ir una funcion (funcion aletorio)
+
+
+int main(){
+	int cant, max; //cant:cantidad y max: maximo 
+	cout<<"Ingrese la cantidad de numeros aletorios";
+	cin>>cant;
+	cout<<"Ingrese el valor maximo permitido";
+	cin>>max;
+	
+	if(cant<=0 || max< 0){
+		cout<<"La cantidad y maximo tienen que ser positivos"<<endl;
+	}else {
+		//funcion (funcion aletorio) 
+	}
+	return 0;
+}
