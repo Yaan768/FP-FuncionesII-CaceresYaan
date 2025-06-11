ab58cc43a210ae69a2aa4183c88b60f8bbc71595
Yaan768 <yaan7682019@gmail.com>
2025-06-10 19:05:16 -0500

    Agregue el ejercicio 1: primer avance

---
+++ ejercicio1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmacht>
+// la funcion tendria q estar por aca mas o menos, solo q no me acuerdo ahorita como era 
+using namespace std; 
+
+int main () {
+	double r; // r= radio 
+	cout<<"Ingrese la longitud del radio del circulo: ";
+	cin>>r; 
+	if (r<0) {
+		cout<<"el radio no puede ser negativo. Vuelva a ingresarlo"<<endl;
+	} else {
+		// aca seria una funcion o un procedimiento pa q me de un resultado xd
+		cout<< "el area del circulo es: "<<area<<endl;  
+	}
+    return 0 
+}
+
