c7e17bb12cf68f6465d761702fd59bcf9f7dfeba
Yaan768 <yaan7682019@gmail.com>
2025-06-16 09:09:11 -0500

    }

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <cstdlib>  
+#include <ctime>    
+using namespace std;
 
-//Aca debe ir una funcion (funcion aletorio)
+// Función para generar y mostrar números aleatorios
+void generarNumerosAleatorios(int cantidad, int maximo) {
+    // hora actual aletoria
+    srand(time(0));
 
-
-int main(){
-	int cant, max; //cant:cantidad y max: maximo 
-	cout<<"Ingrese la cantidad de numeros aletorios";
-	cin>>cant;
-	cout<<"Ingrese el valor maximo permitido";
-	cin>>max;
-	
-	if(cant<=0 || max< 0){
-		cout<<"La cantidad y maximo tienen que ser positivos"<<endl;
-	}else {
-		//funcion (funcion aletorio) 
-	}
-	return 0;
+    cout << "\nNumeros aleatorios generados:\n";
+    for (int i = 0; i < cantidad; i++) {
+        int numero = rand() % (maximo + 1); // Número entre 0 y maximo
+        cout << numero << " ";
+    }
+    cout << endl;
 }
+
+int main() {
+    int cantidad, maximo;
+    //entrada
+    cout << "Ingrese la cantidad de numeros aleatorios a generar: ";
+    cin >> cantidad;
+
+    cout << "Ingrese el valor maximo permitido: ";
+    cin >> maximo;
+
+    if (cantidad <= 0 || maximo < 0) {
+        cout << "Error: cantidad y maximo deben ser positivos." << endl;//salida
+    } else {
+        generarNumerosAleatorios(cantidad, maximo);
+    }
+
+    return 0;
+}
+
